31506f048facd661014000152ab15ba639119aec
Aubreyhet <2930603106@qq.com>
2020-12-31 15:58:44 +0800

    配置后端接口地址

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/apis': {     //这里最好有一个 /
-                target: 'http://127.0.0.1:3000',  // 后台接口域名
+                target: 'http://47.92.222.168:3000',  // 后台接口域名
                 changeOrigin: true,  //是否跨域
                 pathRewrite: {
                     '^/apis': ''
